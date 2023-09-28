d7de6fd9987f11a3d99305aa45ddd464f73089ed
Chilcotin <winniethepooh194@gmail.com>
2023-09-28 23:12:11 +0300

    Create icon. Include dagger hilt in project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -39,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
---
+++ app/src/main/java/com/chilcotin/familyapp/App.kt
@@ -0,0 +1,8 @@
+package com.chilcotin.familyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+}
