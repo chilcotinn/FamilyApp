f8ecd62bd86b94e80423ab9a28fbceb2d4ae8011
Chilcotin <winniethepooh194@gmail.com>
2023-09-29 16:49:05 +0300

    Locked rotate screen (temporarily). Added navigation Bar, background image.

--- app/src/main/java/com/chilcotin/familyapp/MainActivity.kt
+++ app/src/main/java/com/chilcotin/familyapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chilcotin.familyapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chilcotin.familyapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomMenuClick()
+
+    }
+
+    private fun bottomMenuClick() {
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.todo -> Toast.makeText(this, "todo", Toast.LENGTH_SHORT).show()
+                R.id.todoShare -> Toast.makeText(this, "todoShare", Toast.LENGTH_SHORT).show()
+                R.id.shopList -> Toast.makeText(this, "shopList", Toast.LENGTH_SHORT).show()
+                R.id.chat -> Toast.makeText(this, "chat", Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 }
