9846247cbb5d603e50294df6166c1a36aa322688
Chilcotin <winniethepooh194@gmail.com>
2023-10-03 19:43:18 +0300

    Settings back stack. Delete onCreate fun in fragments.

--- app/src/main/java/com/chilcotin/familyapp/fragments/todo/TodoFragment.kt
+++ app/src/main/java/com/chilcotin/familyapp/fragments/todo/TodoFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.chilcotin.familyapp.R
 import com.chilcotin.familyapp.databinding.FragmentTodoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,16 +15,20 @@ class TodoFragment : Fragment() {
     private var _binding: FragmentTodoBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTodoBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fbAddTask.setOnClickListener {
+            findNavController().navigate(R.id.action_todoFragment_to_newTodoFragment, null)
+        }
     }
 
     override fun onDestroy() {
