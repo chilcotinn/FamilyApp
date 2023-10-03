3f445e17e4385c093192b0146558c985b0264e9b
Chilcotin <winniethepooh194@gmail.com>
2023-10-03 21:37:06 +0300

    Resize screen on launch keyboard. Create TodoAdapter.

---
+++ app/src/main/java/com/chilcotin/familyapp/db/TodoAdapter.kt
@@ -0,0 +1,45 @@
+package com.chilcotin.familyapp.db
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.chilcotin.familyapp.R
+import com.chilcotin.familyapp.entity.TodoItem
+
+class TodoAdapter(private val dataSetTodo: List<TodoItem>) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    private var todoListNote = emptyList<TodoItem>()
+
+    class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val title: TextView
+        val description: TextView
+        val time: TextView
+        val checkBox: CheckBox
+
+        init {
+            title = view.findViewById(R.id.tvTitle)
+            description = view.findViewById(R.id.tvDescription)
+            time = view.findViewById(R.id.tvTime)
+            checkBox = view.findViewById(R.id.checkBox)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.title.text = dataSetTodo[position].title
+        holder.description.text = dataSetTodo[position].description
+        holder.time.text = dataSetTodo[position].time
+        holder.checkBox.isChecked = dataSetTodo[position].isChecked
+    }
+
+    override fun getItemCount() = todoListNote.size
+}
