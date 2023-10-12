05d84eb361e2ab39074f524d5c69336a6ecbb7f3
Chilcotin <winniethepooh194@gmail.com>
2023-10-12 19:40:45 +0300

    Create ShareTodoItem entity. Settings MainDb. README.md update.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FamilyApp
+
 Telegram: https://t.me/chilcotin
+
+MinSdk 23
+
+Include:
+
+- Room;
+- Android navigation component;
+- Dagger hilt;
+- LivaData;
+- ViewModel;
+- LeakCanary.
+
+Application:
+
+- First screen - ToDo list. Offline Todo list. Remove items by swiping. Cancel button returns the
+  TodoItem (SnakeBar).
--- app/src/main/java/com/chilcotin/familyapp/db/MainDb.kt
+++ app/src/main/java/com/chilcotin/familyapp/db/MainDb.kt
@@ -2,10 +2,14 @@ package com.chilcotin.familyapp.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.chilcotin.familyapp.entity.ShareTodoItem
 import com.chilcotin.familyapp.entity.TodoItem
 
 @Database(
-    entities = [TodoItem::class],
+    entities = [
+        TodoItem::class,
+        ShareTodoItem::class
+    ],
     version = 1,
     exportSchema = true,
 )
