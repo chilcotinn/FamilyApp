2eaca11b6362dbc2240322fdaadc1f7a07b22b83
chilcotin <winniethepooh194@gmail.com>
2023-10-30 22:41:03 +0300

    Created ShopItemAdapter.

--- app/src/main/java/com/chilcotin/familyapp/adapters/ShopItemAdapter.kt
+++ app/src/main/java/com/chilcotin/familyapp/adapters/ShopItemAdapter.kt
@@ -1,4 +1,59 @@
 package com.chilcotin.familyapp.adapters
 
-class ShopItemAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.chilcotin.familyapp.databinding.ShopItemBinding
+import com.chilcotin.familyapp.entities.ShopItem
+
+class ShopItemAdapter(private val listener: OnItemClickListener) :
+    ListAdapter<ShopItem, ShopItemAdapter.ShopItemViewHolder>(ItemComparator()) {
+
+    inner class ShopItemViewHolder(private val binding: ShopItemBinding) :
+        ViewHolder(binding.root) {
+
+        init {
+            binding.apply {
+                checkBox.setOnClickListener {
+                    val position = adapterPosition
+                    if (position != RecyclerView.NO_POSITION) {
+                        val shopItem = getItem(position)
+                        listener.onCheckedBoxClick(shopItem, checkBox.isChecked)
+                    }
+                }
+            }
+        }
+
+        fun setData(shopItem: ShopItem) {
+            binding.apply {
+                tvTitle.text = shopItem.title
+                checkBox.isChecked = shopItem.isChecked
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val binding = ShopItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ShopItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.setData(currentItem)
+    }
+
+    class ItemComparator : DiffUtil.ItemCallback<ShopItem>() {
+        override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean =
+            oldItem == newItem
+    }
+
+    interface OnItemClickListener {
+        fun onCheckedBoxClick(shopItem: ShopItem, isChecked: Boolean)
+    }
 }
--- app/src/main/java/com/chilcotin/familyapp/fragments/shopList/ShopItemsFragment.kt
+++ app/src/main/java/com/chilcotin/familyapp/fragments/shopList/ShopItemsFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chilcotin.familyapp.App
+import com.chilcotin.familyapp.adapters.ShopItemAdapter
 import com.chilcotin.familyapp.databinding.FragmentShopItemsBinding
+import com.chilcotin.familyapp.entities.ShopItem
+import com.chilcotin.familyapp.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ShopItemsFragment : Fragment() {
+class ShopItemsFragment : Fragment(), ShopItemAdapter.OnItemClickListener {
     private var _binding: FragmentShopItemsBinding? = null
     private val binding get() = _binding!!
+    private val adapter by lazy { ShopItemAdapter(this) }
+
+    private val mainViewModel: MainViewModel by activityViewModels {
+        MainViewModel.MainViewModelFactory((context?.applicationContext as App).database)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +32,23 @@ class ShopItemsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRcView()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun initRcView() = with(binding) {
+        rcShopItems.layoutManager = LinearLayoutManager(requireContext())
+        rcShopItems.adapter = adapter
+        rcShopItems.setHasFixedSize(true)
+    }
+
+    override fun onCheckedBoxClick(shopItem: ShopItem, isChecked: Boolean) {
     }
 }
