676e10d4a16419bc47433061bd7ab42cf38a0dcd
Chilcotin <winniethepooh194@gmail.com>
2023-11-10 21:45:51 +0300

    Up gradle plugin version. Fixed display problem with date in todoItem.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,14 @@ plugins {
 }
 
 buildscript {
+    val agpVersion by extra("8.1.2")
     repositories {
         google()
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:$agpVersion")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
